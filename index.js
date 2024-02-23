cd0fca073c1aa55ab353fe63eddce0c4f9300bda
Abhishek <147073711+CodewithAbhi14@users.noreply.github.com>
2024-02-23 23:37:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const passwordbox = document.getElementById("Password");
+const length = 12;
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_-[]{}<>/?~=";
+const allchars = uppercase + lowercase + symbol + number;
+
+function createpassword(){
+    let  password = "";
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allchars[Math.floor(Math.random() * allchars.length)];
+    }
+    document.getElementById("Password").value = password;
+};
+
+function copypassword(){
+    passwordbox.select();
+    document.execCommand("copy");
+};
